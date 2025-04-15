05cc2a82620c472f7c26d2b199c7da00ef182b40
Artie <artierosser@gmail.com>
2025-04-15 11:18:38 +0000

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   server: { host: true },
   site: "https://arrtie.github.io",
   base: "/pomer-doro",
+
   devToolbar: {
     enabled: false,
   },
+
+  integrations: [react()],
 });
